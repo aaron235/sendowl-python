bc6abdaff2fccbd76425fe502fd67adfe5a208e2
Aaron Adler <aaron.roy.adler@gmail.com>
2019-04-17 21:28:37 -0700

    Add proper header for getting JSON content

--- sendowl_python/sendowl.py
+++ sendowl_python/sendowl.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from typing import List, Dict, Union
 
 
@@ -10,22 +11,26 @@ class SendOwl:
 		self._baseUrl = f"https://{self._key}:{self._secret}@www.sendowl.com/api/v1/"
 
 	def _apiRequest(self, method: str, path: str, params: Dict[str, str] = None) -> Union[List, Dict]:
+		headers = {'Accept': 'application/json'}
 		method = method.lower()
 		url = f'{self._baseUrl}{path}'
 		if method == 'get':
 			if params:
-				req = requests.get(path, params=params)
+				req = requests.get(url, params=params, headers=headers)
 			else:
-				req = requests.get(path)
+				req = requests.get(url, headers=headers)
 		elif method == 'post':
-			req = requests.post(url, data=params)
+			req = requests.post(url, data=params, headers=headers)
 		elif method == 'put':
-			req = requests.put(url, data=params)
+			req = requests.put(url, data=params, headers=headers)
 		elif method == 'delete':
-			req = requests.delete(url)
+			req = requests.delete(url, headers=headers)
 		else:
 			raise ValueError(f'Invalid request method "{method}"')
-		return req.json()
+		try:
+			return req.json()
+		except json.JSONDecodeError:
+			raise RuntimeError(f'Response returned from SendOwl: {req.text}')
 
 	def get_products(self) -> Union[List, Dict]:
 		return self._apiRequest('get', 'products')
